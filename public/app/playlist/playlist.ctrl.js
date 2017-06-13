1620a0a333aeed57db6fcadd3ef0cc5e4b0022d7
MICHELLE ALLRED <michelleallred1@u.boisestate.edu>
2017-06-13 15:26:17 -0600

    Returning Playlist Keys Functional

--- public/app/playlist/playlist.ctrl.js
+++ public/app/playlist/playlist.ctrl.js
@@ -26,6 +26,22 @@
         
         activate();
         
+        //who knows what this is
+        $('.nav-tabs-dropdown').each(function(i, elm) {
+            $(elm).text($(elm).next('ul').find('li.active a').text());
+        });
+
+        $('.nav-tabs-dropdown').on('click', function(e) {
+            e.preventDefault();
+            $(e.target).toggleClass('open').next('ul').slideToggle();
+        });
+
+        $('#nav-tabs-wrapper a[data-toggle="tab"]').on('click', function(e) {
+            e.preventDefault();
+            $(e.target).closest('ul').hide().prev('a').removeClass('open').text($(this).text());
+        });
+        //
+        
         function activate(){
             //get playlist and song array from database
             //songlist and playlist
