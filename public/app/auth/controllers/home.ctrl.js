07ae912a27ac222aacdde7ddb62390c8e7b4fef1
Patrick Cullings <patrick.cullings@gmail.com>
2017-06-13 10:45:35 -0600

    added registerModal and loginModal functions

--- public/app/auth/controllers/home.ctrl.js
+++ public/app/auth/controllers/home.ctrl.js
@@ -30,8 +30,36 @@
 			});
 		}
 
-		
+		function loginModal() {
+			var modal = document.getElementById('loginModal');
+			var btn = document.getElementById("loginModalButton");
+			var span = document.getElementsByClassName("close")[0];
+			btn.onclick = function() {
+			    modal.style.display = "block";
+			}
+			span.onclick = function() {
+			    modal.style.display = "none";
+			}
+			window.onclick = function(event) {
+			    if (event.target == modal)
+			        modal.style.display = "none";
+			}
+		}
+
+		function registerModal() {
+			var modal = document.getElementById('registerModal');
+			var btn = document.getElementById("registerModalButton");
+			var span = document.getElementsByClassName("close")[0];
+			btn.onclick = function() {
+			    modal.style.display = "block";
+			}
+			span.onclick = function() {
+			    modal.style.display = "none";
+			}
+			window.onclick = function(event) {
+			    if (event.target == modal)
+			        modal.style.display = "none";
+			}
+		}
 	}
-
-
 })();
