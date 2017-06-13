7b45e73546df2a6220b27e47088d9218af784603
MICHELLE ALLRED <michelleallred1@u.boisestate.edu>
2017-06-13 11:55:02 -0600

    Changed index.html and routes.js

--- public/app/playlist/playlist.routes.js
+++ public/app/playlist/playlist.routes.js
@@ -11,12 +11,8 @@
         $stateProvider
             .state('playlist', {
                 url: '/playlist',
-<<<<<<< HEAD
-                template: '<playlist></playlist>'
-=======
                 templateUrl: 'app/playlist/playlist.tpl.html',
                 controller: 'PlaylistCtrl'
->>>>>>> aa77030cdefbd6d26504385fdba5184b641ef55c
         });            
     }    
 })();
