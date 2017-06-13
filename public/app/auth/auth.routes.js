0ec2ade67bac49f9281c6d8978a3173418b2c146
Jacobi <jgraziano2@gmail.com>
2017-06-13 10:09:54 -0600

    Home template commit

---
+++ public/app/auth/auth.routes.js
@@ -0,0 +1,18 @@
+(function() {
+	'use strict';
+
+	angular
+		.module('app')
+		.config(config);
+
+	config.$inject = ['$stateProvider'];
+
+	function config($stateProvider) {
+		$stateProvider
+			.state('home', {
+				url: '/home',
+				templateUrl: 'app/auth/templates/home.tpl.html',
+				controller: 'HomeCtrl'
+			};
+	}
+})();
