fb99a2ab1c87d8a504cd5e67650280be98684bfe
Jacobi <jgraziano2@gmail.com>
2017-06-14 12:00:26 -0600

    Added all sorts of awesome functionality to playlist control

--- public/app/playlist/playlist.ctrl.js
+++ public/app/playlist/playlist.ctrl.js
@@ -1,12 +1,12 @@
 (function (){
     'use strict'
-    
+
     angular
         .module('app')
         .controller('PlaylistCtrl', PlaylistCtrl);
-    
+
     PlaylistCtrl.$inject = ['$scope', '$firebaseArray', '$stateParams', '$firebaseObject'];
-    
+
     function PlaylistCtrl($scope, $firebaseArray, $stateParams, $firebaseObject){       
         $scope.playlists;        
         $scope.editPlaylists = editPlaylists;
@@ -14,86 +14,76 @@
         $scope.savePlaylists = savePlaylists;
         $scope.removePlaylist = removePlaylist;
         $scope.selectPlaylist = selectPlaylist;
-        
-        $scope.loadSongs = loadSongs;
+
         $scope.userSongs;
-        $scope.songs = [];
         $scope.editSongs = editSongs;
         $scope.addSong = addSong;        
         $scope.saveSonglist = saveSonglist;
         $scope.removeSong = removeSong;        
-        
+
         $scope.isEditingPlaylists = false;
         $scope.isEditingSongs = false;
-        
-        var curPlaylistId = 0;
-        
+
+        $scope.curPlaylistId = 0;
+
         activate();       
-        
+
         function activate(){
             var ref = firebase.database().ref().child($stateParams.id).child('playlists');            
             $scope.playlists = $firebaseArray(ref);
-            
+
+            $scope.playlists.$loaded().then(function(data) {
+                $scope.curPlaylistId = $scope.playlists[0].$id;
+            });
+
             var ref = firebase.database().ref().child($stateParams.id).child('songs');  
             $scope.userSongs = $firebaseArray(ref);   
-            loadSongs();
         }
-        
+
         function editPlaylists(){
             $scope.isEditingPlaylists = true;
         }
-        
+
         function addPlaylist(){
             $scope.playlists.$add({name: 'New Playlist'});
-            curPlaylistId = $scope.playlists[0].$id;
+            $scope.curPlaylistId = $scope.playlists[0].$id;
         }
-        
+
         function removePlaylist(playlist){
             $scope.playlists.$remove(playlist);
         }
-        
+
         function savePlaylists(){
             for(var i=0; i<$scope.playlists.length; i++){
-             $scope.playlists.$save($scope.playlists[i]);   
+                $scope.playlists.$save($scope.playlists[i]);   
             }
             $scope.isEditingPlaylists = false;
         }
-        
+
         function selectPlaylist(playlist){
-            $scope.songs=[];
-            curPlaylistId = playlist.$id;    
-            loadSongs();
+            $scope.curPlaylistId = playlist.$id;  
+            console.log($scope.curPlaylistId);
         }   
-        
+
         function editSongs(){
             $scope.isEditingSongs = true;
         }
-        
+
         function addSong(){  
-            var newSong = {name: "New Song", url: "", playlistId: curPlaylistId};
+            var newSong = {name: "New Song", url: "", playlistId: $scope.curPlaylistId};
             $scope.userSongs.$add(newSong);
-            $scope.userSongs.$loaded().then(function(data){
-                console.log("Loading songs...")
-                loadSongs();
-            });            
         }
-        
+
         function removeSong(song){
-            $scope.userSongs.$remove(song.$id);
+            $scope.userSongs.$remove(song);
         }        
-            
-        function saveSonglist(){            
-            $scope.isEditingSongs = false;
-            loadSongs();
-        }
-        
-        function loadSongs(){
+
+        function saveSonglist(){
             for(var i=0; i<$scope.userSongs.length; i++){
-                if($scope.userSongs[i].playlistId === curPlaylistId){
-                    $scope.songs.push($scope.userSongs[i]);    
-                }             
-            } 
+                $scope.userSongs.$save($scope.userSongs[i]);   
+            }
+            $scope.isEditingSongs = false;
         }
     }
-    
+
 })();
