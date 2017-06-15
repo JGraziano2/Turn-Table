9a3a2cc66644764258b7d83c44477030557d7c8f
Zach Richardson <zach@tyrocoders.com>
2017-06-15 17:07:30 -0600

    Made some stuff wayyy better. Totally fucked up other things

--- public/app/playlist/playlist.ctrl.js
+++ public/app/playlist/playlist.ctrl.js
@@ -5,55 +5,60 @@
         .module('app')
         .controller('PlaylistCtrl', PlaylistCtrl);
     
-    PlaylistCtrl.$inject = ['$scope', '$firebaseArray', '$stateParams', '$firebaseObject', '$window', 'ModalService', 'PlaylistService', '$sce'];
+    PlaylistCtrl.$inject = ['$window','$scope', '$stateParams', '$firebaseObject', '$firebaseArray', '$sce', 'ModalService', 'PlaylistService', 'AuthService'];
 
-    function PlaylistCtrl($scope, $firebaseArray, $stateParams, $firebaseObject, $window, ModalService, PlaylistService, $sce){        
-
-        $scope.playlists;        
-        $scope.editPlaylists = editPlaylists;
-        $scope.addPlaylist = addPlaylist;
-        $scope.savePlaylists = savePlaylists;
-        $scope.removePlaylist = removePlaylist;
-        $scope.selectPlaylist = selectPlaylist;
-
+    function PlaylistCtrl($window, $scope, $stateParams, $firebaseObject, $firebaseArray, $sce, ModalService, PlaylistService, AuthService){        
         $scope.logout = logout;
-        
-        $scope.displayVid = displayVid;
-        $scope.userSongs;
-        $scope.editSongs = editSongs;
-        $scope.addSong = addSong;        
-        $scope.saveSonglist = saveSonglist;
-
-        $scope.moveSongUp = moveSongUp;
-        $scope.moveSongDown = moveSongDown;
-        
-        $scope.removeSong = removeSong; 
-        $scope.video;
-        $scope.currSong;
-        $scope.playNext = playNext;
-        $scope.playPrev = playPrev;
-
         $scope.isEditingPlaylists = false;
         $scope.isEditingSongs = false;
 
-        $scope.curPlaylistId = 0;
-        var playlistLength;
-        $scope.currID;
+        /*-- PLAYLISTS --*/
+        $scope.playlists; 
+        $scope.currentPlaylist;  
+        $scope.addPlaylist = addPlaylist;     
+        $scope.editPlaylists = editPlaylists;
+        $scope.removePlaylist = removePlaylist;
+        $scope.savePlaylists = savePlaylists;
+        $scope.selectPlaylist = selectPlaylist;
+
+        /*-- SONGS --*/
+        $scope.songs;
+        $scope.currentSong;
+        $scope.addSong = addSong;
+        $scope.editSongs = editSongs; 
+        $scope.moveSongUp = moveSongUp;
+        $scope.moveSongDown = moveSongDown; 
+        $scope.removeSong = removeSong; 
+        $scope.saveSongs = saveSongs;
+
+        /*-- VIDEOS --*/ 
+        $scope.video;
+        $scope.displayVid = displayVid;  
+        $scope.playNext = playNext;
+        $scope.playPrev = playPrev;
+
 
         activate();
 
         function activate(){
             var ref = firebase.database().ref().child($stateParams.id).child('playlists');            
             $scope.playlists = $firebaseArray(ref);
+            selectPlaylist($scope.playlists[0]);
 
             var ref = firebase.database().ref().child($stateParams.id).child('songs');  
-            $scope.userSongs = $firebaseArray(ref); 
-            $scope.userSongs.$loaded().then(function(data) {
-                for(var i = 0; i < $scope.userSongs.length; i++){
-                    if($scope.userSongs[i].playlistId == $scope.playlists[0].$id && $scope.userSongs[i].index == 1){
-                        displayVid($scope.userSongs[i]);
-                    }                       
-                }
+            $scope.songs = $firebaseArray(ref);
+            for(var i = 0; i < $scope.songs.length; i++){
+                if(isFoundSong($scope.songs[i], 1))
+                    displayVid($scope.songs[i]);                    
+            }
+        }
+
+        /*-- PLAYLIST METHODS --*/
+
+        function addPlaylist(){
+            $scope.playlists.$add({name: 'New Playlist', length: 0})
+            .then((data) => {
+                $scope.currentPlaylist.$id = data[0].$id;
             });
         }
 
@@ -61,162 +66,124 @@
             $scope.isEditingPlaylists = true;
         }
 
-        function addPlaylist(){
-            $scope.playlists.$loaded().then(function(data){
-                $scope.playlists.$add({name: 'New Playlist', length: 0});
-                $scope.curPlaylistId = $scope.playlists[0].$id;
-            });
-            activate();
-        }
-
         function removePlaylist(playlist){
-            for(var i=0; i<$scope.userSongs.length; i++){
-                if($scope.userSongs[i].playlistId==playlist.$id) $scope.userSongs.$remove($scope.userSongs[i]);
+            for(var i=0; i<$scope.songs.length; i++){
+                if($scope.songs[i].playlistId == playlist.$id) 
+                    $scope.songs.$remove($scope.songs[i]);
             }
             $scope.playlists.$remove(playlist);
         }
 
         function savePlaylists(){
-            for(var i=0; i<$scope.playlists.length; i++){
-                $scope.playlists.$save($scope.playlists[i]);   
-            }
+            $scope.playlists.forEach((playlist) => {                
+                $scope.playlists.$save(playlist); 
+            });
             $scope.isEditingPlaylists = false;
         }
 
         function selectPlaylist(playlist){
-            $scope.curPlaylistId = playlist.$id;  
-            playlistLength = playlist.length;
-        }   
+            $scope.currentPlaylist = playlist;
+        }
+
+        /*-- SONG METHODS --*/
+
+        function addSong(){
+            $scope.currentPlaylist.length++; 
+            $scope.songs.$add({
+                name: "New Song", 
+                url: "", 
+                playlistId: $scope.currentPlaylist.$id, 
+                index: $scope.currentPlaylist.length
+            });           
+        }  
 
         function editSongs(){
             $scope.isEditingSongs = true;
         }
 
-        function addSong(){
-            var playlist;
-            $scope.playlists.$loaded().then( function(data){
-                for(var i=0; i<$scope.playlists.length; i++){
-                    if($scope.playlists[i].$id===$scope.curPlaylistId){
-                        playlist=$scope.playlists[i];
-                        $scope.playlists[i].length++;
-                        playlistLength = $scope.playlists[i].length;
-                        savePlaylists();
-                    }  
-                }
-                $scope.userSongs.$add({name: "New Song", url: "", playlistId: $scope.curPlaylistId, index:playlist.length});
-            });
-        }
-
-        function removeSong(song){
-            var playlist;
-            $scope.playlists.$loaded().then( function(data){
-                for(var i=0; i<$scope.playlists.length; i++){
-                    if($scope.playlists[i].$id===$scope.curPlaylistId){
-                        playlist=$scope.playlists[i];
-                        $scope.playlists[i].length--;
-                        playlistLength = $scope.playlists[i].length;
-                        savePlaylists();
-                    }  
-                }
-            $scope.userSongs.$remove(song);
-            });
-        }        
-
-        function saveSonglist(){
-            for(var i=0; i<$scope.userSongs.length; i++){
-                $scope.userSongs.$save($scope.userSongs[i]);   
-            }
-            $scope.isEditingSongs = false;
-        }
-
-        function moveSongUp(song){
-            var curSongId = song.$id, curSongIndex = song.index;
-            var prevSongId, prevSongIndex;            
-            if(curSongIndex!=1){ 
-                for(var i=0; i<$scope.userSongs.length; i++){
-                    if($scope.userSongs[i].index==curSongIndex-1){
-                        prevSongId = $scope.userSongs[i].$id;
-                        prevSongIndex = $scope.userSongs[i].index;
-                    }
-                }
-                for(var i=0; i<$scope.userSongs.length; i++){
-                    if($scope.userSongs[i].$id==curSongId) $scope.userSongs[i].index = prevSongIndex;
-                    if($scope.userSongs[i].$id==prevSongId) $scope.userSongs[i].index = curSongIndex;
-                }                              
-            }else{
-                console.log("Out of bounds movement attempt!!!!")
-            }
+        function moveSongUp(song) {
+            if (song.index == 1) return;
+            var previousSong = findSong(song.index-1);
+            swapSongs(song, previousSong);            
         }
 
         function moveSongDown(song){
-            var curSongId = song.$id, curSongIndex = song.index;
-            var nextSongId, nextSongIndex;            
-            if(curSongIndex!=playlistLength){ 
-                for(var i=0; i<$scope.userSongs.length; i++){
-                    if($scope.userSongs[i].index==curSongIndex+1){
-                        nextSongId = $scope.userSongs[i].$id;
-                        nextSongIndex = $scope.userSongs[i].index;
-                    }
+            if(song.index >= $scope.currentPlaylist.length) return;
+            var nextSong = findSong(song.index+1);
+            swapSongs(song, nextSong);
+        }
+
+        function removeSong(song){
+            $scope.currentPlaylist.length--;
+            $scope.songs.$remove(song); 
+        }        
+
+        function saveSongs(){
+            $scope.songs.forEach((song) => {
+                $scope.songs.$save(song);
+            });
+            $scope.isEditingSongs = false;
+        }
+
+        /*-- VIDEO METHODS --*/
+
+        function displayVid(song) {
+            $scope.currentSong = song;
+            $scope.video = $sce.trustAsResourceUrl(song.url);
+        }
+        
+        function playNext(){
+            if($scope.currentSong.index >= $scope.currentPlaylist.length) return;
+            for(var i = 0; i < $scope.songs.length; i++){
+                if(isFoundSong($scope.songs[i], $scope.currentSong.index+1)) {
+                    displayVid($scope.currentSong);
+                    break;
                 }
-                for(var i=0; i<$scope.userSongs.length; i++){
-                    if($scope.userSongs[i].$id==curSongId) $scope.userSongs[i].index = nextSongIndex;
-                    if($scope.userSongs[i].$id==nextSongId) $scope.userSongs[i].index = curSongIndex;
-                }                              
-            }else{
-                console.log("Out of bounds movement attempt!!!!")
-            }
+            } 
+        }
+        
+        function playPrev(){
+            if($scope.currentSong.index <= 1) return;
+            for(var i = 0; i < $scope.songs.length; i++){
+                if(isFoundSong($scope.songs[i], $scope.currentSong.index-1)) {
+                    displayVid($scope.currentSong);
+                    break;
+                }
+            } 
         }
 
         function logout(){
-            firebase.auth().signOut().then(function() {
+            AuthService.logout().then(() => {
                 $window.location.href = '/';
-            }, function(error) {
+            }, (error) => {
                 console.log(error);
             });
         }
 
-        function displayVid(song) {
-            $scope.currID = song.$id;
-            PlaylistService.setID(song.url);
-            $scope.video = $sce.trustAsResourceUrl(PlaylistService.getID());
-            $scope.currSong = song.url;
-        }
-        
-        function playNext(){
-            var currPlaylistID, currSongIndex;
-            for(var i = 0; i<$scope.userSongs.length; i++){
-                if($scope.userSongs[i].$id == $scope.currID){
-                    currPlaylistID = $scope.userSongs[i].playlistId;
-                    currSongIndex = $scope.userSongs[i].index;
-                }
-            }
-            if(currSongIndex != playlistLength){
-                for(var i = 0; i < $scope.userSongs.length; i++){
-                    if($scope.userSongs[i].index == currSongIndex+1 && $scope.userSongs[i].playlistId == currPlaylistID){
-                        displayVid($scope.userSongs[i]);
-                        break;
-                    }
-                }                
-            }
-        }
-        
-        function playPrev(){
-            var currPlaylistID, currSongIndex;
-            for(var i = 0; i<$scope.userSongs.length; i++){
-                if($scope.userSongs[i].$id == $scope.currID){
-                    currPlaylistID = $scope.userSongs[i].playlistId;
-                    currSongIndex = $scope.userSongs[i].index;
-                }
-            }
+        /*-- HELPER METHODS --*/
 
-            if(currSongIndex != 1){
-                for(var i = 0; i < $scope.userSongs.length; i++){
-                    if($scope.userSongs[i].index == currSongIndex-1 && $scope.userSongs[i].playlistId == currPlaylistID){
-                        console.log('we here');
-                        displayVid($scope.userSongs[i]);
-                        break;
-                    }
+        function isFoundSong(song, songIndex) {
+            return song.playlistId == $scope.currentPlaylist.$id && song.index == songIndex;
+        }
+
+        function findSong(songIndex) {
+            var song = {};
+            for(var i=0; i<$scope.songs.length; i++){          
+                if($scope.songs[i].index == songIndex) {
+                    song.id = $scope.songs[i].$id;
+                    song.index = $scope.songs[i].index;
+                    return song;
                 }
+            }
+            return song;
+        }
+
+        function swapSongs(song, swap) {
+            for(var i=0; i<$scope.songs.length; i++){
+                if($scope.songs[i].$id == song.$id) 
+                    $scope.songs[i].index = swap.index;
+                if($scope.songs[i].$id == swap.$id) 
+                    $scope.songs[i].index = song.index;
             }
         }
         
